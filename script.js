be850e17449bd957bc1fca7b8f6405cf016c4a35
Ayako Woollan <ayako.woollan@gmail.com>
2021-12-04 13:07:52 +1100

    updating html and script

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
 var wins = 0;
 var ties = 0;
 var loses = 0;
-var options = ["R", "P", "S"]
+var options = ["R", "P", "S"];
 
 
-if (confirm("Are you ready?")) {
+function playGame() {
+    var userInput = prompt("Enter 'R' or 'P'or 'S'");
 
-    function playGame() {
-        var userInput = prompt("Enter 'R' or 'P'or 'S'");
-
-        if (!userInput) {
-            return;
-        }
-
-        userInput = userInput.toUpperCase();
-        var random = Math.floor(Math.random() * options.length);
-        var computerInput = option[random];
-        alert("The computer chose " + computerInput);
-
-        if (userInput === computerInput) {
-            ties++;
-            alert("It's a tie!");
-            var tryAgain = confirm("Try again?");
-            if (tryAgain) {
-                playGame();
-            } else {
-                alert("Bye bye");
-            }
-
-        } else if (
-            (userInput === "R" && computerInput === "S") ||
-            (userInput === "P" && computerInput === "R") ||
-            (userInput === "S" && computerInput === "P")
-        ) {
-            wins++;
-            window.alert("You win!");
-
-            if (tryAgain) {
-                playGame();
-            }
-
-        } else {
-            loses++;
-            alert("You lost!");
-            if (tryAgain) {
-                playGame();
-            }
-
-            window.alert(
-                "Stats:\nWins: " + wins + "\nTies: " + ties + "\nLosses: " + losses
-            );
-
-
-        };
+    if (!userInput) {
+        return;
     }
 
-} else {
-    alert("bye bye");
-};
+    userInput = userInput.toUpperCase();
+    var random = Math.floor(Math.random() * options.length);
+    var computerInput = options[random];
+    alert("The computer chose " + computerInput);
+
+    if (userInput === computerInput) {
+        ties++;
+        alert("It's a tie!\nStats:\nWins: " + wins + "\nTies: " + ties + "\nLosses: " + loses);
+
+        if (confirm("Try again?")) {
+            playGame();
+        } else {
+            byeBye;
+        }
+
+    } else if (
+        (userInput === "R" && computerInput === "S") ||
+        (userInput === "P" && computerInput === "R") ||
+        (userInput === "S" && computerInput === "P")
+    ) {
+        wins++;
+        alert("You win!\nStats:\nWins: " + wins + "\nTies: " + ties + "\nLosses: " + loses);
+
+        if (confirm("Try again?")) {
+            playGame();
+        } else {
+            byeBye;
+        }
+
+    } else {
+        loses++;
+        alert("You lost!\nStats:\nWins: " + wins + "\nTies: " + ties + "\nLosses: " + loses);
+
+        if (confirm("Try again?")) {
+            playGame();
+        } else {
+            byeBye;
+        }
+
+    };
+
+
+}
+
+playGame();
